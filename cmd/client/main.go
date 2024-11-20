cad455284bc3be5e292fba1deff67111c354b393
itsmeberwyn <itsmeberwyn@hireme.com>
2024-11-20 13:59:56 +0800

    :art: detect pieces war movement

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -34,7 +34,8 @@ func main() {
 	pubsub.SubscribeJSON(conn, routing.ExchangePerilDirect, routing.PauseKey+"."+user, routing.PauseKey, transient,
 		handlerPause(gameState))
 	pubsub.SubscribeJSON(conn, routing.ExchangePerilTopic, routing.ArmyMovesPrefix+"."+user, routing.ArmyMovesPrefix+".*",
-		transient, handlerMove(gameState))
+		transient, handlerMove(ch, gameState))
+	pubsub.SubscribeJSON(conn, routing.ExchangePerilTopic, "war", "war.*", durable, handlerWar(ch, gameState))
 	for {
 		input := gamelogic.GetInput()
 		switch input[0] {
@@ -77,7 +78,7 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) string {
 	}
 }
 
-func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) string {
+func handlerMove(ch *amqp.Channel, gs *gamelogic.GameState) func(gamelogic.ArmyMove) string {
 	defer fmt.Print(">")
 	return func(ga gamelogic.ArmyMove) string {
         outcome := gs.HandleMove(gamelogic.ArmyMove{
@@ -85,11 +86,50 @@ func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) string {
 			Units:      ga.Units,
 			ToLocation: ga.ToLocation,
 		})
-        if (outcome == gamelogic.MoveOutComeSafe) || (outcome == gamelogic.MoveOutcomeMakeWar) {
+        if outcome == gamelogic.MoveOutComeSafe {
             return "Ack"
-        } 
+        }
+        if outcome == gamelogic.MoveOutcomeMakeWar {
+            err := pubsub.PublishJSON(ch, routing.ExchangePerilTopic, 
+                routing.WarRecognitionsPrefix + "." + gs.GetUsername(),
+                gamelogic.RecognitionOfWar{
+                    Attacker: gs.Player,
+                    Defender: ga.Player,
+                })
+            if err != nil {
+                log.Fatal(err)
+            }
+            return "NackRequeue"
+        }
         return "NackDiscard"
 	}
+}
+
+func handlerWar(ch *amqp.Channel, gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) string {
+    defer fmt.Print(">")
+    return func(rec gamelogic.RecognitionOfWar) string {
+        outcome, _, _ := gs.HandleWar(rec)
+        log.Println(outcome, " hello 123")
+        if outcome == gamelogic.WarOutcomeNotInvolved {
+            return "NackRequeue"
+        } else if outcome == gamelogic.WarOutcomeNoUnits {
+            return "NackDiscard"
+        } 
+
+        if outcome == gamelogic.WarOutcomeOpponentWon {
+            log.Println("WarOutcomeOpponentWon")
+            return "Ack"
+        }
+        if outcome == gamelogic.WarOutcomeYouWon {
+            log.Println("WarOutcomeYouWon")
+            return "Ack"
+        } 
+        if outcome == gamelogic.WarOutcomeDraw {
+            log.Println("WarOutcomeDraw")
+            return "Ack"
+        }
+        return "NackDiscard"
+    }
 }
 
 func publishMessage[T any](ch *amqp.Channel, exchange string, key string, message T) error {
